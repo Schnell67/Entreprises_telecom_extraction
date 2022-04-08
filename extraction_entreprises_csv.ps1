2f99a75e3d5f4f04cd74e8af15e83080300296f9
Lucas SCHNELL <lucasschnell32@gmail.com>
2022-04-08 12:39:15 +0200

    Update extraction_entreprises_csv.ps1

--- extraction_entreprises_csv.ps1
+++ extraction_entreprises_csv.ps1
@@ -1,6 +1,6 @@
 ﻿$chemin=pwd
 wget "https://extranet.arcep.fr/portail/Portals/0/Documents/Arcep/IdentifiantsCE/identifiants_CE.csv" -OutFile $chemin\identifiants_CE.csv
 $liste = Import-Csv -Path $chemin\identifiants_CE.csv -Delimiter ";" | 
-Where-Object {$_.RCS_ACTEUR -match "(Strasbourg)"} |
+Where-Object {$_.RCS_ACTEUR -match "(Ville1 | Ville2 | Ville3 ...)"} |
 Select-Object -Property IDENTITE_OPERATEUR, RCS_ACTEUR, ADRESSE_COMPLETE_ACTEUR |
 Export-Csv -Path $chemin\resultat_liste_entreprises.csv -Delimiter  ";" -Encoding UTF8
